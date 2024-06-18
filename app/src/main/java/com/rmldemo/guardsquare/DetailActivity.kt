c902c557e159fee5371fb34569cb07194816b89d
zenfikry <zenfikry@gmail.com>
2024-06-18 19:06:32 +0700

    fixing viewBinding

--- app/src/main/java/com/rmldemo/guardsquare/DetailActivity.kt
+++ app/src/main/java/com/rmldemo/guardsquare/DetailActivity.kt
@@ -1,10 +1,7 @@
 package com.rmldemo.guardsquare
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.bumptech.glide.Glide
 import com.rmldemo.guardsquare.databinding.ActivityDetailBinding
 
@@ -14,14 +11,7 @@ class DetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
-        activityDetailBinding = ActivityDetailBinding.inflate(LayoutInflater.from(applicationContext))
+        activityDetailBinding = ActivityDetailBinding.inflate( layoutInflater )
         setContentView(activityDetailBinding.root)
 
         setSupportActionBar(findViewById(R.id.toolbar))
--- app/src/main/java/com/rmldemo/guardsquare/MainActivity.kt
+++ app/src/main/java/com/rmldemo/guardsquare/MainActivity.kt
@@ -3,11 +3,8 @@ package com.rmldemo.guardsquare
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.rmldemo.guardsquare.api.ApiService
@@ -25,14 +22,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
-        activityMainBinding = ActivityMainBinding.inflate( LayoutInflater.from(applicationContext) )
+        activityMainBinding = ActivityMainBinding.inflate( layoutInflater )
         setContentView(activityMainBinding.root)
     }
 
